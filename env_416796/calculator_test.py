77cd3b5ecfd45f8335baeadcd5a15b574586c58e
wojferda <148353841+wojferda@users.noreply.github.com>
2025-06-22 22:42:53 +0200

    Update calculator_test.py

--- env_416796/calculator_test.py
+++ env_416796/calculator_test.py
@@ -1,4 +1,5 @@
-from calculator import dodawanie,dzielenie,mnozenie,odejmowanie
+import pandas as pd
+from calculator import dodawanie,dzielenie,mnozenie,odejmowanie, suma_kolumny, srednia_kolumny
 def test_dodawanie():
     assert dodawanie(2, 3) == 5
     assert dodawanie(-1, 1) == 0
@@ -15,3 +16,11 @@ def test_dzielenie():
     assert dzielenie(6, 3) == 2
     assert dzielenie(1, -1) == -1
     assert dzielenie(1, 0) == "Błąd: dzielenie przez zero!"
+
+def test_suma_kolumny():
+    assert suma_kolumny([1, 2, 3, 4]) == 10
+    assert suma_kolumny([]) == 0
+
+def test_srednia_kolumny():
+    assert srednia_kolumny([1, 2, 3]) == 2
+    assert pd.isna(srednia_kolumny([]))
